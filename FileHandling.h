a007090e0462cb44e0a72de496843b9f5eb5db9a
nicolewkn <u3626774@connect.hku.hk>
2024-12-03 18:28:25 +0800

    Update FileHandling.h

--- FileHandling.h
+++ FileHandling.h
@@ -4,13 +4,15 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
 class Game_setting;
 
 class FileHandling {
 public:
     static void openFile(Game_setting& game);
-    static void writeFile(Game_setting& game, std::string data);
-    static void appendToFile(Game_setting& game, std::string text);
+    static void writeFile(Game_setting& game, string data);
+    static void appendToFile(Game_setting& game, string text);
     static void closeFile(Game_setting& game);
     static void readFile(Game_setting& game);
 };
