377082a2f7ae148028fc3be5953ae471bda54ff2
Chen Ni <u3626780@connect.hku.hk>
2024-12-03 18:24:26 +0800

    Create GameSetting.h

---
+++ GameSetting.h
@@ -0,0 +1,62 @@
+#ifndef GAME_SETTING_H
+#define GAME_SETTING_H
+
+#include <map>
+#include <vector>
+#include <string>
+#include <utility>
+using namespace std;
+
+class Game_setting {
+public:
+    void initialize_game();
+    void start_of_day();
+    void perform_action();
+    void exploreIsland();
+    void cookFood();
+    void eatFood();
+    void craftItem();
+    void rest();
+    void hunting();
+    void attempt_escape();
+    void print_bag();
+    string weather_ran();
+    pair<int, int> deduction();
+    int check_health();
+    bool check_gun_bullet_blueprint();
+    bool check_signal_flare_blueprint();
+    bool check_armor_blueprint();
+    void blueprint_list();
+    void wildAnimalAttack();
+    void poor_sleeping_quality();
+    void night_event();
+    void openFile();
+    void writeFile(string data);
+    void appendToFile(string text);
+    void readFile();
+    void closeFile();
+
+private:
+    int day;
+    int HP;
+    int Hunger;
+    int Mental;
+    int step_remains;
+    map<string, int> bag;
+    vector<string> blueprints;
+    string weather;
+    ofstream outFile;
+
+    struct Node {
+        string name;
+        Node* next;
+    };
+
+    void delete_list(Node*& head);
+
+    extern map<string, int> bag;
+    extern vector<string> blueprints;
+    extern string weather;
+};
+
+#endif
