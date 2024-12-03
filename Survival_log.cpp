ad293a6fefc977682a9b2719f903c5f8fd005952
nicolewkn <u3626774@connect.hku.hk>
2024-12-03 21:51:11 +0800

    Update Survival_log.cpp

--- Survival_log.cpp
+++ Survival_log.cpp
@@ -1,18 +1,18 @@
-#include "Survival_log.h"
+#include "SurvivalLog.h"
 #include "GameSetting.h"
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-void Survival_log::openFile(Game_setting& game) {
+void SurvivalLog::openFile(Game_setting& game) {
     game.outFile.open("Survival Log");
     if (!game.outFile) {
         cerr << "Error opening file!" << endl;
     }
 }
 
-void Survival_log::writeFile(Game_setting& game, std::string data) {
+void SurvivalLog::writeFile(Game_setting& game, std::string data) {
     if (game.outFile.is_open()) {
         game.outFile << data << endl;
     }
@@ -21,7 +21,7 @@ void Survival_log::writeFile(Game_setting& game, std::string data) {
     }
 }
 
-void Survival_log::appendToFile(Game_setting& game, std::string text) {
+void SurvivalLog::appendToFile(Game_setting& game, std::string text) {
     ofstream outFile("Survival Log", ios::app);
     if (!outFile) {
         cerr << "Error opening file for appending!" << endl;
@@ -31,13 +31,13 @@ void Survival_log::appendToFile(Game_setting& game, std::string text) {
     outFile.close();
 }
 
-void Survival_log::closeFile(Game_setting& game) {
+void SurvivalLog::closeFile(Game_setting& game) {
     if (game.outFile.is_open()) {
         game.outFile.close();
     }
 }
 
-void Survival_log::readFile(Game_setting& game) {
+void SurvivalLog::readFile(Game_setting& game) {
     ifstream inFile("Survival Log");
     if (!inFile) {
         cerr << "Error opening file: " << "Survival Log" << endl;
