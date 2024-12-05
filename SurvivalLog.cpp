c89e97aade8572e2cea8b56e81550b74782156ef
nicolewkn <u3626774@connect.hku.hk>
2024-12-05 15:50:25 +0800

    Update SurvivalLog.cpp

--- SurvivalLog.cpp
+++ SurvivalLog.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
+//Opens a file named "Survival Log" for writing output.
+//Inputs: Game_setting& game - A reference to a Game_setting object that contains an ofstream member named outFile.
 void SurvivalLog::openFile(Game_setting& game) {
     game.outFile.open("Survival Log");
-    if (!game.outFile) {
+    if (!game.outFile) { //Checks if the file was successfully opened and prints an error message if it was not.
         cerr << "Error opening file!" << endl;
     }
-}
+} //No return value. Prints an error message to cerr if the file cannot be opened.
 
 void SurvivalLog::writeFile(Game_setting& game, string data) {
     if (game.outFile.is_open()) {
